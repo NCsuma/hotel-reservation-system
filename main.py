7bfa1c41d2b249e9c461ac4af08b52722ef3b939
NCsuma <52526716+NCsuma@users.noreply.github.com>
2021-08-29 06:54:31 +0530

    Updated header

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+    File name: main.py
+    Author: Suma N C
+    Date created: 29/08/2021
+    Date last modified: 29/08/2021
+    Python Version: 3.1.6
+"""
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
